50725585210590753aff856460c79247afcd9504
PatrickJS <github@gdi2290.com>
2016-07-23 16:56:59 -0700

    refactor: update style

--- index.js
+++ index.js
@@ -2,10 +2,21 @@
   MIT License http://www.opensource.org/licenses/mit-license.php
   Author Tobias Koppers @sokra
 */
-var JSON5 = require("json5");
-module.exports = function(source) {
-  this.cacheable && this.cacheable();
-  var value = JSON5.parse(source);
+var JSON5 = require('json5');
+module.exports = function Json5Plugin (source) {
+  if (this.cacheable) {
+    this.cacheable();
+  }
+
+  var value = undefined;
+
+  try {
+    value = JSON5.parse(source);
+  } catch (e) {
+    throw new Error('Error using JSON5 parsing')
+  }
+
   this.values = [value];
-  return "module.exports = " + JSON.stringify(value, undefined, "\t");
+
+  return 'module.exports = ' + JSON.stringify(value, null, '\t');
 }
