b923cbfaffc01c379d25bdb6f0e136942aa7b10b
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-06-05 15:55:15 +0300

    chore(deps): update (#40)
    BREAKING CHANGE: minimum required nodejs version is `8.9.0`

---
+++ test/helpers/compiler.js
@@ -0,0 +1,75 @@
+import path from 'path';
+
+import del from 'del';
+import webpack from 'webpack';
+import MemoryFS from 'memory-fs';
+
+const moduleConfig = (config) => {
+  return {
+    rules: config.rules
+      ? config.rules
+      : [
+          {
+            test: (config.loader && config.loader.test) || /\.json5$/i,
+            use: [
+              {
+                loader: path.resolve(__dirname, '../../src/index.js'),
+                options: (config.loader && config.loader.options) || {},
+              },
+            ],
+          },
+        ],
+  };
+};
+const pluginsConfig = (config) => [].concat(config.plugins || []);
+const outputConfig = (config) => {
+  return {
+    path: path.resolve(
+      __dirname,
+      `../outputs/${config.output ? config.output : ''}`
+    ),
+    filename: '[name].bundle.js',
+  };
+};
+
+function compile(fixture, config = {}, options = {}) {
+  // webpack Config
+  // eslint-disable-next-line no-param-reassign
+  config = {
+    mode: 'development',
+    devtool: config.devtool || 'sourcemap',
+    context: path.resolve(__dirname, '../fixtures'),
+    entry: path.resolve(__dirname, '../fixtures', fixture),
+    output: outputConfig(config),
+    module: moduleConfig(config),
+    plugins: pluginsConfig(config),
+    optimization: {
+      runtimeChunk: true,
+    },
+  };
+
+  // Compiler Options
+  // eslint-disable-next-line no-param-reassign
+  options = Object.assign({ output: false }, options);
+
+  if (options.output) {
+    del.sync(config.output.path);
+  }
+
+  const compiler = webpack(config);
+
+  if (!options.output) {
+    compiler.outputFileSystem = new MemoryFS();
+  }
+
+  return new Promise((resolve, reject) =>
+    compiler.run((error, stats) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(stats);
+    })
+  );
+}
+
+export default compile;
--- test/loader.test.js
+++ test/loader.test.js
@@ -1,118 +1,50 @@
+import fs from 'fs';
+import path from 'path';
+
 import JSON5 from 'json5';
 
 import loader from '../src';
+
+import webpack from './helpers/compiler';
 
 describe('loader', () => {
   it('should export the loader', () => {
     expect(loader).toBeInstanceOf(Function);
   });
 
-  it('should handle valid JSON5', () => {
-    const json5 = `{
-  // comments
-  unquoted: 'and you can quote me on that',
-  singleQuotes: 'I can use "double quotes" here',
-  lineBreaks: "Look, Mom! \\
-No \\\\n's!",
-  hexadecimal: 0xdecaf,
-  leadingDecimalPoint: .8675309, andTrailing: 8675309.,
-  positiveSign: +1,
-  trailingComma: 'in objects', andIn: ['arrays',],
-  "backwardsCompatible": "with JSON",
-}`;
-    const emitError = jest.fn();
-    const content = loader.call(
-      {
-        emitError,
-      },
-      json5
-    );
+  it('should handle valid JSON5', async () => {
+    const testId = './test.json5';
+    const stats = await webpack(testId);
+    const { modules } = stats.toJson();
+    const module = modules.find((m) => m.id === testId);
 
-    expect(content).toMatchSnapshot();
+    expect(module.source).toMatchSnapshot('module');
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
+
+    const content = fs
+      .readFileSync(path.resolve(__dirname, './fixtures/test.json5'))
+      .toString();
 
     // eslint-disable-next-line no-eval
-    const js = eval(content);
-
-    expect(JSON5.stringify(js)).toEqual(JSON5.stringify(JSON5.parse(json5)));
+    expect(eval(module.source)).toEqual(JSON5.parse(content));
   });
 
-  it('should invalid characters', () => {
-    const json5 = `{foo: '\u2028\u2029'}`;
-    const emitError = jest.fn();
-    const content = loader.call(
-      {
-        emitError,
-      },
-      json5
-    );
+  it('should handle invalid JSON5', async () => {
+    const testId = './broken.json5';
+    const stats = await webpack(testId);
 
-    expect(content).toMatchSnapshot();
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(
+      stats.compilation.errors.map((error) => {
+        // eslint-disable-next-line no-param-reassign
+        error.message = error.message.replace(
+          /\(from .*?\)/i,
+          '(replaced/path)'
+        );
 
-    // eslint-disable-next-line no-eval
-    const js = eval(content);
-
-    expect(JSON5.stringify(js)).toEqual(JSON5.stringify(JSON5.parse(json5)));
-  });
-
-  it('should preserve Infinity', () => {
-    const json5 = '{to : Infinity}';
-    const content = loader.call({}, json5);
-
-    expect(content).toMatchSnapshot();
-
-    // eslint-disable-next-line no-eval
-    const js = eval(content);
-
-    expect(JSON5.stringify(js)).toEqual(JSON5.stringify(JSON5.parse(json5)));
-  });
-
-  it('should preserve -Infinity', () => {
-    const json5 = '{to : -Infinity}';
-    const content = loader.call({}, json5);
-
-    expect(content).toMatchSnapshot();
-
-    // eslint-disable-next-line no-eval
-    const js = eval(content);
-
-    expect(JSON5.stringify(js)).toEqual(JSON5.stringify(JSON5.parse(json5)));
-  });
-
-  it('should preserve null', () => {
-    const json5 = '{null : null}';
-    const content = loader.call({}, json5);
-
-    expect(content).toMatchSnapshot();
-
-    // eslint-disable-next-line no-eval
-    const js = eval(content);
-
-    expect(JSON5.stringify(js)).toEqual(JSON5.stringify(JSON5.parse(json5)));
-  });
-
-  it('should preserve NaN', () => {
-    const json5 = '{nan : NaN}';
-    const content = loader.call({}, '{nan : NaN}');
-
-    expect(content).toMatchSnapshot();
-
-    // eslint-disable-next-line no-eval
-    const js = eval(content);
-
-    expect(JSON5.stringify(js)).toEqual(JSON5.stringify(JSON5.parse(json5)));
-  });
-
-  it('should handle invalid JSON5', () => {
-    const brokenJson5 = '{broken: json5}';
-    const emitError = jest.fn();
-
-    loader.call(
-      {
-        emitError,
-      },
-      brokenJson5
-    );
-
-    expect(emitError).toHaveBeenCalledWith(expect.any(SyntaxError));
+        return error;
+      })
+    ).toMatchSnapshot('errors');
   });
 });
